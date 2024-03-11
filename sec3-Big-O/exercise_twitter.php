4754db618d9177e22d44757b43d2ce1393afec4f
Zichun Wang <zwang2@wvstateu.edu>
2024-03-10 22:18:40 -0400

    finished chap3

---
+++ sec3-Big-O/exercise_twitter.php
@@ -0,0 +1,16 @@
+<?php
+// Find 1st, Find Nth...
+
+//compare each tweet with each one.
+//O(n^2)
+$array =
+ [array("tweet"=> "hi", "date" => 2012),
+ array("tweet" => "my", "date" => 2014),
+ array("tweet" => "teddy", "date" => 2018)];
+
+
+ //O(1)
+ //ref:https://stackoverflow.com/questions/5292325/algorithmic-complexity-of-php-function-strlen
+ echo strlen("hellowhaleflaskdrive");
+ 
+
