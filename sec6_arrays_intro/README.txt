1050e716c2a1ac432a0666b6410e7ebf3d49229a
Zichun Wang <zwang2@wvstateu.edu>
2024-03-14 17:23:41 -0400

    finsihed class 77th

--- sec6_arrays_intro/README.txt
+++ sec6_arrays_intro/README.txt
@@ -6,3 +6,6 @@ course 71st: Implementing An Array <--- i_an_array.php
 course 73rd & 74th: Exercise: Reverse A String <-- reverse.php 
 course 75th & 76th: Exercise: Merge Sorted Arrays <-- merge.php
 (working on now)
+
+Leetcode Questions:
+Q1, Q53, Q283, Q217, Q189
--- sec6_arrays_intro/merge.php
+++ sec6_arrays_intro/merge.php
@@ -1,2 +1,67 @@
 <?php
 //Merge two sorted arrays into one single array:
+
+
+function mergeSortedArrays($array1, $array2){
+    $mergedArray = array();
+    $array1Item = $array1[0];
+    if(gettype($array1) != "array"){
+        throw new Exception("The type of lhs is not array");
+    }
+    if(gettype($array2) != "array"){
+        throw new Exception("The type of rhs is not array");
+    }
+    $length1 = sizeof($array1);
+    $length2 = sizeof($array2);
+
+    if($length1 == 0){
+        return $array2;
+    }
+
+    if($length2 == 0){
+        return $array1;
+    }
+
+    $mergedArray = array();
+    
+    $array1Item = $array1[0];
+    $array2Item = $array2[0];
+    $i = $j = 0;
+    while($i < $length1 && $j < $length2)
+    {
+            if($array1[$i] < $array2[$j]){
+                array_push($mergedArray, $array1[$i]);
+                $i++;
+            }
+            else {
+                array_push($mergedArray, $array2[$j]);
+                $j++;
+            }
+        }
+      
+    //deal with remaining elements:
+    if($i < $length1){
+        for(;$i<$length1;$i++){
+            array_push($mergedArray,$array1[$i]);
+        }
+    }
+   if($j < $length2){
+    for(;$j < $length2;$j++){
+        array_push($mergedArray,$array2[$j]);
+    }
+   }
+   return $mergedArray;
+}
+
+  
+$arr_ichi = [1,3,5,7,9];
+$arr_ni = [-7,-3,2,4,6,8,10,13,15,17];
+$new_arr = mergeSortedArrays($arr_ichi, $arr_ni);
+print_r($new_arr);
+$new_arr1 = mergeSortedArrays($arr_ichi, []);
+print_r($new_arr1);
+try{
+mergeSortedArrays($arr_ichi, 32);
+}catch(Exception $e){
+    echo $e->getMessage();
+}
