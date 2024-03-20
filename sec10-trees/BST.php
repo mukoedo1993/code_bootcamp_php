2ea1bb96479f6530ee050ec05dc72d677edb752d
Zichun Wang <zwang2@wvstateu.edu>
2024-03-19 20:16:40 -0400

    prepare BST

--- sec10-trees/BST.php
+++ sec10-trees/BST.php
@@ -1,4 +1,1 @@
-<?php //https://medium.com/the-andela-way/binary-tree-implementation-in-php-e12df09d046f
-//https://stackoverflow.com/a/20709158
-//https://gist.github.com/meetrajesh/4570999
-//https://stackoverflow.com/questions/20709055/php-arrays-what-is-are-the-meanings-of-an-empty-bracket
+<?php //https://gist.github.com/thinkphp/1448754/9408efdd433c7c01b7eda17ad808ace53f4c3a38
