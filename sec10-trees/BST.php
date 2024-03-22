5c30ff3f11ec888b6a927229d3a7db3e903ac3ae
Zichun Wang <zwang2@wvstateu.edu>
2024-03-21 20:25:37 -0400

    update all BST files

--- sec10-trees/BST.php
+++ sec10-trees/BST.php
@@ -1,1 +1,14 @@
-<?php //https://gist.github.com/thinkphp/1448754/9408efdd433c7c01b7eda17ad808ace53f4c3a38
+<?php
+//ref: https://gist.github.com/meetrajesh/4570999
+
+class BinarySearchTree {
+    private $_root;
+    public function _construct() {
+        //setup sentinal node
+        $this->_root = new BinarySearchNode(null);
+    }
+
+    public function getRoot() {
+        return $this->hasNode() ? $this->_root->right : null;
+    }
+}
--- sec10-trees/README.txt
+++ sec10-trees/README.txt
@@ -1,1 +1,14 @@
 BST.php -> for implementation of BST in PHP language
+
+Tree Questions:
+Q94:
+Q100:
+Q102:
+Q814:
+Q112:
+Q129:
+Q236:
+Q297:
+Q508:
+Q124:
+Q968:
