3dcd510d42b4abfa2d71d011337349703dc65915
Zichun Wang <zwang2@wvstateu.edu>
2024-03-28 21:53:42 -0400

    finish string questions

--- sec12-recursion/string.php
+++ sec12-recursion/string.php
@@ -1,17 +1,32 @@
 <?php
-function reverserString($str){
+//solution: https://replit.com/@aneagoie/reverseString
+function reverseString($str){
     //type check first
     if(gettype($str) != "string"){
         throw new Exception("Not a string!");
     }
+        $arr1 = str_split($str);
+        $arr2 = [];
+        while(false == empty($arr1)){
+            $top = array_pop($arr1);
+            array_push($arr2, $top);
+        }
+        $new_string = implode($arr2);
+        return $new_string;
 }
-reverseString("yoyo master");
+
+echo reverseString("yoyo master")."\n";
 
 function reverseStringRecursive($str) {
     //type check first
     if(gettype($str) != "string"){
         throw new Exception("Not a string!");
     }
-
+    //then, do check the length of string:
+    if(strlen($str) == 0 || strlen($str) == 1){
+        return $str;
+    }
+  
+    return (reverseStringRecursive(substr($str, 1))).$str[0];
 }
-reverseStringRecursive("yoyo master");
+echo reverseStringRecursive("yoyo master");
