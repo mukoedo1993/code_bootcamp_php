ffe69beb3a50a54f152f85d22f415a3eee2133d4
Zichun Wang <zwang2@wvstateu.edu>
2024-04-01 22:00:30 -0400

    update bfs and dfs

---
+++ sec14-searching-dfs-bfs/bfsANDdfs.py
@@ -0,0 +1,141 @@
+#Binary search tree operations in Python
+
+#Create a node
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+# Inorder traversal
+def inorder(root):
+    if root is not None:
+        # Traverse left
+        inorder(root.left)
+
+        #Traverse root
+        print(f'{str(root.key)}->', end=' ')
+
+        # Traverse right
+        inorder(root.right)
+
+# Insert a node
+def insert(node, key):
+
+    # Return a new node if the tree is empty
+    if node is None:
+        return Node(key)
+    
+    # Traverse to the right place and insert the node
+    if key < node.key:
+        node.left = insert(node.left, key)
+    elif key > node.key:
+        node.right = insert(node.right, key)
+    else:
+        pass
+    return node
+
+# Find the inorder successor
+def minValueNode(node):
+    current = node
+
+    # Find the leftmost leaf
+    while(current.left is not None):
+        current = current.left
+
+        return current
+    
+    
+# Deleting a node:
+def deleteNode(root, key):
+
+    # Return if the tree is empty
+    if root is None:
+        return root
+    
+    # Find the node to be deleted
+    if key < root.key:
+        root.left = deleteNode(root.left, key)
+    elif(key > root.key):
+        root.right = deleteNode(root.right, key)
+    else:
+        # If the node is with only one child or no chile
+        if root.left is None:
+            temp = root.right
+            root = None
+            return temp
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+        
+        # If the node has two children,
+        # place the inorder successor in position of the node to be deleted
+        temp = minValueNode(root.right)
+
+        root.key = temp.key
+
+        # Delete the inorder successor
+        root.right = deleteNode(root.right, temp.key)
+    
+    return root
+
+#DFS traversal
+def DFS(root,result:list):
+    if root is None:return None
+    result.append(root.key)
+    if root.left != None:
+        DFS(root.left, result)
+    if root.right != None:
+        DFS(root.right, result)
+    
+
+
+# BFS traversal:
+
+def BFS(root, result:list):
+    if root is None:return None
+    result.append(root.key)
+    result1 = []
+
+    while(len(result) > 0):
+        cur_node = result.pop(0)
+        result1.append(cur_node)
+
+        if cur_node.left is not None:
+            result.append(cur_node.left)
+        if cur_node.right is not None:
+            result.append(cur_node.right)
+
+    return cur_node
+
+        
+        
+    
+
+
+    
+
+
+root = None
+root = insert(root, 8)
+root = insert(root, 3)
+root = insert(root, 1)
+root = insert(root, 6)
+root = insert(root, 7)
+root = insert(root, 10)
+root = insert(root, 14)
+root = insert(root, 4)
+
+temp = []
+BFS(root,temp)
+print(temp)
+
+print("Inorder traversal: ", end=' ')
+inorder(root)
+
+print("\nDelete 10")
+root = deleteNode(root, 10)
+print("Inorder traversal: ", end=' ')
+inorder(root)
+
